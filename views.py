28d37f827356c083fdcf72e781e8aac53c2e2429
Christian <christianbrintnall@gmail.com>
2017-09-12 01:28:04 -0700

    Add user list for shift manager

--- views.py
+++ views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+import shiftmanager.settings
 
 def index(request):
     context = {}
+    context["creating_shifts"] = shiftmanager.settings.CREATING_SHIFTS;
+
+    context['user_list'] = get_user_model().objects.all()
 
     return render(
         request,
