27e4ffaecf638da10f818091c691411059e7e0da
Christian <christianbrintnall@gmail.com>
2017-09-13 12:16:49 -0700

    Add functionality to allow certain users to choose shifts

--- views.py
+++ views.py
@@ -3,10 +3,17 @@ from django.contrib.auth import get_user_model
 import shiftmanager.settings
 
 def index(request):
+    user_model = get_user_model()
+
     context = {}
     context["creating_shifts"] = shiftmanager.settings.CREATING_SHIFTS;
+    context['user_list'] = user_model.objects.all()
 
-    context['user_list'] = get_user_model().objects.all()
+    if request.method == 'POST' and request.POST.get('create_shifts', False):
+        for user in request.POST.keys():
+            if request.POST.get(user, False) == 'on':
+                curr_user = user_model.objects.get(username=user)
+                print(curr_user.first_name)
 
     return render(
         request,
