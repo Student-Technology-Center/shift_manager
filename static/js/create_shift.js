bfcb171d83593e4f97014937ac44b3b76cb7167d
Christian <christianbrintnall@gmail.com>
2017-09-20 17:54:29 -0700

    Allow sending ajax request to backend for filling out shift object

--- static/js/create_shift.js
+++ static/js/create_shift.js
@@ -4,4 +4,24 @@ $(document).ready(function(){
         weekNumbers: false,
         firstDay: 1,
     });
+    $('#submit_shift').click(function() {
+
+        var day = document.getElementById('id_day_of_week').value
+        var start = document.getElementById('id_start').value
+        var end = document.getElementById('id_end').value
+
+        $.ajax({
+            url: '/shifts/api/add_shift/',
+            type: 'POST',
+            data: {
+                "csrfmiddlewaretoken": csrf_token,
+                "day": day,
+                "start": start,
+                "end": end
+            },
+            success: function(data) {
+                console.log(data);
+            }
+        })
+    });
 })
--- views.py
+++ views.py
@@ -3,19 +3,22 @@ from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.db import IntegrityError
 
-from shiftmanager.models import ShiftPlacement
-from random import randint
+from shiftmanager.models import ShiftPlacement, ShiftForm
+
+import random
 
 def index(request):
     user_model = get_user_model()
 
     context = {}
     context["creating_shifts"] = settings.CREATING_SHIFTS;
-    context['user_list'] = user_model.objects.all()
 
     if not context['creating_shifts']:
         if request.method == 'POST' and request.POST.get('create_shifts', False) and request.user.is_superuser:
-            num_of_users = len([x for x in request.POST.keys() if request.POST.get(x, False) == 'on'])
+            settings.USER_SHIFT_PLACE = 1
+            user_list = [x for x in request.POST.keys() if request.POST.get(x, False) == 'on']
+            user_obj_list = [user_model.objects.get(username=x) for x in user_list]
+            num_of_users = len(user_list)
 
             if num_of_users == 0:
                 context['no_users'] = True
@@ -25,27 +28,23 @@ def index(request):
                     context
                 )
 
-            user_num_list = []
-            for user in request.POST.keys():
-                if request.POST.get(user, False) == 'on':
-                    settings_obj = ShiftPlacement.objects.get_or_create(user=user_model.objects.get(username=user))
-                    rand_num = randint(0, num_of_users - 1)
+            for user in user_obj_list:
+                user.shiftplacement.order = 0
+                user.shiftplacement.save()
+            
+            list_of_placement = random.sample(range(1, num_of_users + 1), num_of_users)
 
-                    while rand_num in user_num_list:
-                        rand_num = randint(0, num_of_users - 1)
-                    
-                    user_num_list.append(rand_num)
-                    try:
-                        settings_obj[0].order = rand_num
-                        settings_obj[0].save()
-                    except IntegrityError:
-                        pass
+            for index, user in enumerate(user_obj_list):
+                user.shiftplacement.order = list_of_placement[index]
+                user.shiftplacement.save()
+
             settings.CREATING_SHIFTS = True
             return redirect('/shifts/create/')
+
+    context['user_list'] = user_model.objects.all()
     
     if settings.CREATING_SHIFTS:
         return redirect('/shifts/create/')
-        
 
     return render(
         request,
@@ -56,10 +55,13 @@ def index(request):
 def shift_page(request):
     context = {}
     context['creating_shifts'] = settings.CREATING_SHIFTS
+    context['shift_form'] = ShiftForm(None)
 
-    if request.method == 'POST':
-        settings.CREATING_SHIFTS = False
+    user_model = get_user_model()
+    if not context['creating_shifts']:
         return redirect('/shifts/')
+    context['starting_place'] = settings.USER_SHIFT_PLACE
+    context['current_user'] = ShiftPlacement.objects.get(order=settings.USER_SHIFT_PLACE).user
 
     return render(
             request,
