725c97dc9c52bbca943ea1186e6fd157b255de33
STC <STC@hhd191-112.atus.wwu.edu>
2017-09-27 18:42:02 -0700

    Fix merge issues

--- views.py
+++ views.py
@@ -15,15 +15,11 @@ def index(request):
 
     if not context['creating_shifts']:
         if request.method == 'POST' and request.POST.get('create_shifts', False) and request.user.is_superuser:
-<<<<<<< HEAD
-            num_of_users = len([x for x in request.POST.keys() if request.POST.get(x, False) == 'on'])
-=======
             settings.USER_SHIFT_PLACE = 1
             user_list = [x for x in request.POST.keys() if request.POST.get(x, False) == 'on']
             user_obj_list = [user_model.objects.get(username=x) for x in user_list]
             num_of_users = len(user_list)
 
->>>>>>> bfcb171d83593e4f97014937ac44b3b76cb7167d
             if num_of_users == 0:
                 context['no_users'] = True
                 return render(
@@ -42,19 +38,6 @@ def index(request):
                 user.shiftplacement.order = list_of_placement[index]
                 user.shiftplacement.save()
 
-<<<<<<< HEAD
-                    while rand_num in user_num_list:
-                        rand_num = randint(0, num_of_users - 1)
-                    
-                    user_num_list.append(rand_num)
-                    try:
-                        settings_obj[0].order = rand_num
-                        settings_obj[0].save()
-                    except IntegrityError:
-                        pass
-            settings.NUM_USERS = num_of_users
-=======
->>>>>>> bfcb171d83593e4f97014937ac44b3b76cb7167d
             settings.CREATING_SHIFTS = True
             settings.USER_SHIFT_PLACE = 0
             return redirect('/shifts/create/')
